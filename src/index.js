7b1c7388e4e22ade6c1216a9a770f3bfc890a432
Scott Silverman <scottms724@gmail.com>
2019-12-21 16:53:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const taskSubmit = document.querySelectorAll("input[type=submit]")[0];
 
 function handleClick(e) {
   let toDoTask = document.querySelectorAll("input[type=text]")[0].value;
-  if (toDoItem !== "") {
+  if (toDoTask !== "") {
     let el = document.createElement("li");
-    el.innerHTML = toDoItem;
+    el.innerHTML = toDoTask;
     document.getElementById("tasks").appendChild(el);
     document.querySelectorAll("input[type=text]")[0].value = "";
   }
