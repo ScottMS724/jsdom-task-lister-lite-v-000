af8d3d468b3fc2b99b1801583c3a695a18b4ca37
Scott Silverman <scottms724@gmail.com>
2019-12-21 16:53:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const taskSubmit = document.querySelectorAll("input[type=submit]")[0];
 
 function handleClick(e) {
-  let toDoItem = document.querySelectorAll("input[type=text]")[0].value;
+  let toDoTask = document.querySelectorAll("input[type=text]")[0].value;
   if (toDoItem !== "") {
     let el = document.createElement("li");
     el.innerHTML = toDoItem;
